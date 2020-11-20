e8a2c1838dbe3d7af1b32e9ad0b79bc8bfd8d3d3
eliasstalpaertstudent <r0669524@kuleuven.be>
2020-11-20 13:27:55 +0100

    Javadoc

--- CarRental-lib/src/session/ManagerSessionRemote.java
+++ CarRental-lib/src/session/ManagerSessionRemote.java
@@ -8,23 +8,84 @@ import rental.CarType;
 @Remote
 public interface ManagerSessionRemote {
     
+    /**
+     * Get a set containing all rental company names
+     * @return a set of strings
+     * @throws RemoteException 
+     */
     public Set<String> getAllRentalCompanies() throws RemoteException;
     
+    /**
+     * Get a set of all car types provided by a specified car rental company
+     * @param company name of the company
+     * @return a set of CarType objects
+     * @throws RemoteException 
+     */
     public Set<CarType> getCarTypes(String company) throws RemoteException;
     
+    /**
+     * Get a set of the IDs of all cars of a particular type in a company
+     * @param company name of the company
+     * @param type name of the car type
+     * @return a set of Integer objects
+     * @throws RemoteException 
+     */
     public Set<Integer> getCarIds(String company,String type) throws RemoteException;
     
+    /**
+     * Get the number of reservations for a particular car in a car rental company
+     * @param company name of the company
+     * @param type name of the car type
+     * @param carId id of the car
+     * @return int
+     * @throws RemoteException 
+     */
     public int getNumberOfReservations(String company, String type, int carId) throws RemoteException;
     
+    /**
+     * Get the number of reservations for a particular car type in a car rental company
+     * @param company name of the company
+     * @param type name of the car type
+     * @return int
+     * @throws RemoteException 
+     */
     public int getNumberOfReservations(String company, String type) throws RemoteException;
     
+    /**
+     * Add a new company to the car rental agency using a .csv file
+     * @param companyCsv path to .csv file
+     * @throws RemoteException 
+     */
     public void addCompany(String companyCsv) throws RemoteException;
     
+    /**
+     * Remove a company from the car rental agency
+     * @param companyName name of the company to be removed
+     * @throws RemoteException 
+     */
     public void removeCompany(String companyName) throws RemoteException;
-           
+    
+    /**
+     * Get the number of reservations made by a specific renter
+     * @param renter name of the renter
+     * @return int
+     * @throws RemoteException 
+     */
     public int getNumberOfReservationsOfRenter(String renter) throws RemoteException;
     
+    /**
+     * Get a set of all the best clients of the agency
+     * @return a set of String
+     * @throws RemoteException 
+     */
     public Set<String> getBestClients() throws RemoteException;
     
+    /**
+     * Get the most popular car type of a car rental company for a given calendar year
+     * @param carRentalCompanyName name of the company
+     * @param year calendar year
+     * @return CarType object
+     * @throws RemoteException 
+     */
     public CarType getMostPopularCarTypeIn(String carRentalCompanyName, int year) throws RemoteException;
 }
--- CarRental-lib/src/session/ReservationSessionRemote.java
+++ CarRental-lib/src/session/ReservationSessionRemote.java
@@ -13,20 +13,72 @@ import rental.ReservationException;
 @Remote
 public interface ReservationSessionRemote {
     
+    /**
+     * Set name of the renter associated with the session
+     * @param name new name
+     * @throws RemoteException 
+     */
     public void setRenterName(String name) throws RemoteException;
     
+    /**
+     * Get the name of the renter associated with the session
+     * @return string of the name
+     * @throws RemoteException 
+     */
     public String getRenterName() throws RemoteException;
     
+    /**
+     * Get a set containing all rental company names
+     * @return a set of strings
+     * @throws RemoteException 
+     */
     public Set<String> getAllRentalCompanies() throws RemoteException;
     
+    /**
+     * Get a list of all available car types within a specified period
+     * @param start start of the period
+     * @param end end of the period
+     * @return a list of CarType objects
+     * @throws RemoteException 
+     */
     public List<CarType> getAvailableCarTypes(Date start, Date end) throws RemoteException;
     
+    /**
+     * Try to create a quote with the given constraints
+     * @param renter renter name
+     * @param start start of the reservation period
+     * @param end end of the reservation period
+     * @param carType car type to be reserved
+     * @param region reservation region
+     * @return a quote made with a car rental company consistent with the given constraints
+     * @throws RemoteException
+     * @throws ReservationException 
+     */
     public Quote createQuote(String renter, Date start, Date end, String carType, String region) throws RemoteException, ReservationException;
     
+    /**
+     * Get a list of the quotes associated with the client's session
+     * @return a list of Quote objects
+     * @throws RemoteException 
+     */
     public List<Quote> getCurrentQuotes() throws RemoteException;
     
+    /**
+     * Try to confirm all quotes associated with the client's session
+     * @return a list of Reservation objects
+     * @throws RemoteException
+     * @throws ReservationException 
+     */
     public List<Reservation> confirmQuotes() throws RemoteException, ReservationException;
 
+    /**
+     * Get the name of the cheapest car type within a given period
+     * @param start start of the period
+     * @param end end of the period
+     * @param region region of the car type
+     * @return a string containing the name of the car type
+     * @throws RemoteException 
+     */
     public String getCheapestCarType(Date start, Date end, String region) throws RemoteException;
     
 }
